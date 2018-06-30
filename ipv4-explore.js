2371ea682db2ac1a87923a43c27d8f43bab86210
Forrest Smith <fasmith0229@gmail.com>
2018-06-30 09:27:01 -0500

    added IANA general registry information to the Input Summary table as a row.  File iana-registry.js has been added to support.  Added links to all RFCs in the Special Registry table entry.  Added links to all footnotes provided in the IANA registry data.

--- ipv4-explore.js
+++ ipv4-explore.js
@@ -9,6 +9,11 @@ function find_super(data, ip) {
     return(flag);
 }
 
+function find_registry(data, ip) {
+    var ipt = new ipa.Address4(data.Prefix);
+    return(ip.isInSubnet(ipt));
+}
+
 function highlight_net(binstr, prefix) {
     /* highlights characters in binstr according to prefix
        (assumed to be a prefix length)
@@ -73,33 +78,74 @@ function explore() {
     var net_dec = ip.startAddress().address;
     var net_bits = ip.startAddress().getBitsBase2();
 
-    /* Find the parent supernet in the IANA data */
+    /* Find the parent supernet in the IANA special registry data */
     var supernet = iana_data.filter(function(data) { return find_super(data,ip)});
-    if (supernet.length == 2) {
-	/* The only overlapping blocks in the data are for 192.168.0.0/16 and
-	   the NAT64 discovery addresses
-	 */
-	supernet = supernet[1];
+
+    if (supernet === undefined || supernet.length == 0) {
+
+	super_string = "Nothing to show here...";
+
     }
+
     else {
-	supernet = supernet[0];
+	if (supernet.length == 2) {
+	    /* The only overlapping blocks in the data are for 192.168.0.0/16 and
+	       the NAT64 discovery addresses
+	     */
+	    supernet = supernet[1];
+	}
+	else {
+	    supernet = supernet[0];
+	}
+
+	super_string = "";
+	/* Handle RFC Links in special registry data */
+	var m = supernet.RFC.match(/RFC[0-9]+/g);
+	console.log(m);
+	supernet.RFC = ""
+	var k;
+	for (k = 0; k < m.length; k++){
+	    supernet.RFC += "<a target='_blank' ";
+	    supernet.RFC += "href='https://tools.ietf.org/html/" + m[k].toLowerCase() + "'>";
+	    supernet.RFC += "[" + m[k] + "]</a>";
+	}
+
+	for(x in supernet) {
+	    super_string += x + " : " + supernet[x] + "<br>";
+	}
+
+	/* Handle special cases where broadcast, network, first and last
+	   aren't applicable.
+	 */
+	if (supernet.Source == 'False') { render_additional = false}
+
     }
 
-    s = "";
+    /* Find the parent supernet in the IANA registry data */
+    var reg_supernet = registry_data.filter(function(data) { return find_registry(data,ip)})[0];
 
-    for(x in supernet) {
-	s += x + " : " + supernet[x] + "<br>";
+    /* Handle footnote links in the IANA data */
+    if (reg_supernet.Note.length > 0 ) {
+	var k;
+	var m = reg_supernet.Note.match(/[0-9]+/g);
+	reg_supernet.Note = '';
+	for(k = 0; k < m.length; k++) {
+	    reg_supernet.Note += "<a target='_blank' ";
+	    reg_supernet.Note += "href='https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.xml#note";
+	    reg_supernet.Note += m[k] + "'>[" + m[k] + "]</a>";
+	}
+    }
+    registry_string = '';
+    for(x in reg_supernet) {
+	registry_string += x + " : " + reg_supernet[x] + "<br>";
     }
 
     /* Rendering Section */
-    $('#iana-data')[0].innerHTML = s;
+    $('#registry-data')[0].innerHTML = registry_string;
+    $('#iana-data')[0].innerHTML = super_string;
     $('#user-dec')[0].innerText = ip.addressMinusSuffix;
     $('#user-bin')[0].innerHTML = out_bits;
 
-    /* Handle special cases where broadcast, network, first and last
-       aren't applicable.
-     */
-    if (supernet.Source == 'False') { render_additional = false}
 
 
     if (render_additional) {
--- makefile
+++ makefile
@@ -1,9 +1,10 @@
 # Deployment directory
 DEST=/var/www/classapps/static/ipv4explore
 
-deploy: index.html main.js main.css iana-ipv4-special-registry.js ipv4-explore.js
+deploy: index.html main.js main.css iana-ipv4-special-registry.js ipv4-explore.js iana-registry.js
 	cp index.html $(DEST)
 	cp main.css $(DEST)
 	cp main.js $(DEST)
 	cp iana-ipv4-special-registry.js $(DEST)
 	cp ipv4-explore.js $(DEST)
+	cp iana-registry.js $(DEST)
